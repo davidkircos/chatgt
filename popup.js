7965dad9ed87d00dbe056e99766a643fcde08520
David Kircos <davidkircos@gmail.com>
2014-09-20 10:01:40 -0400

    beta 1

--- popup.js
+++ popup.js
@@ -1,7 +1,37 @@
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
-    document.body.innerHTML += tablink;
+// Create a new Firebase reference, and a new instance of the Login client
+var chatRef = new Firebase('https://blazing-inferno-8771.firebaseio.com/chat');
+
+function chatPost(chatRoom) {
+  var past = "";
+  chatRoom.once('value', function(nameSnapshot) {
+    if (nameSnapshot.val()) {
+      past += nameSnapshot.val()
+    }
   });
+  chatRoom.set( "# " + document.getElementById("chatBody").value + '<br>\n' + past);
+  document.getElementById("chatBody").value = "";
+
+  return false;
+}
+
+
+$(document).ready(function() {
+  tablink = "";
+  chrome.tabs.getSelected(null,function(tab) {
+    tablink = new URL(tab.url).hostname.replace('.', '').replace('.', '');
+    console.log( tablink);
+    var chatRoom = chatRef.child( tablink );
+    chatRoom.on('value', function (snapshot) {
+      document.getElementById('chatLog').innerHTML = snapshot.val();
+      console.log(snapshot.val());
+    }, function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
+    });
+
+    $("#btnSubmit").click(function(){
+        chatPost(chatRoom);
+    }); 
+
+  });
+
 });

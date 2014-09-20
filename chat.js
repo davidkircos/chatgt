5a9e296b5d61afb87c1216dabc9f074cda0b4d16
David Kircos <davidkircos@gmail.com>
2014-09-20 13:17:41 -0400

    beta2

--- chat.js
+++ chat.js
@@ -1,37 +1,54 @@
-// Create a new Firebase reference, and a new instance of the Login client
+// Create a new Firebase reference
 var chatRef = new Firebase('https://blazing-inferno-8771.firebaseio.com/chat');
 
+// post a new message to given chatRoom
 function chatPost(chatRoom) {
   var past = "";
   chatRoom.once('value', function(nameSnapshot) {
+    // Take old chat content
     if (nameSnapshot.val()) {
       past += nameSnapshot.val()
     }
   });
-  chatRoom.set( "# " + document.getElementById("chatBody").value + '<br>\n' + past);
-  document.getElementById("chatBody").value = "";
+  // add our new message to it!
+  chatRoom.set( ("# " + document.getElementById("chatBody").value + '<br>\n' + past).substring(0, 1000000) );
 
-  return false;
+  // clear input field
+  document.getElementById("chatBody").value = "";
 }
 
-
 $(document).ready(function() {
+  // initilise url storing variable
   tablink = "";
+
+  // Once we figure out the url for the page we are on 
+  // we can open the chat for it
   chrome.tabs.getSelected(null,function(tab) {
+    // simplify url
     tablink = new URL(tab.url).hostname.replace('.', '').replace('.', '');
-    console.log( tablink);
+    
+    // get chat for url
     var chatRoom = chatRef.child( tablink );
+
+    // on chat update, update view
     chatRoom.on('value', function (snapshot) {
       document.getElementById('chatLog').innerHTML = snapshot.val();
-      console.log(snapshot.val());
     }, function (errorObject) {
+      // fail gracefully! pretty good for a hackathon eh?
       console.log('The read failed: ' + errorObject.code);
     });
 
+    // Tie sending to chat to the button
     $("#btnSubmit").click(function(){
         chatPost(chatRoom);
     }); 
 
   });
 
+  // Prevent Form from reloading page
+  $( "#chatForm" ).submit(function( event ) {
+    event.preventDefault();
+  });
+
 });
+
